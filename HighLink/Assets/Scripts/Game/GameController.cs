df4c023da4c0e773b497cb6a6618e7c87de4c48b
Arnau Fernandez Gil <a22arnfergil@inspedralbes.cat>
2025-03-31 08:21:41 +0200

    added stats control, created more map and changed some materials

--- HighLink/Assets/Scripts/Game/GameController.cs
+++ HighLink/Assets/Scripts/Game/GameController.cs
@@ -9,11 +9,14 @@ public class GameController : MonoBehaviour
     public static GameController Instance { get; private set; }
 
     private string ServerUri = "http://localhost:4000";
+    private string StatsAPIUri = "http://localhost:4001";
     private string GameAPIUri = "/api/games";
+    private string CheckStatsUri = "/state-stats";
 
     private bool ServerOnline = false;
     private bool StatsOnline = false;
 
+    private float heightToShow = 0f;
     private int GameId;
     [SerializeField] private TMP_Text gameIdText;
 
@@ -32,8 +35,13 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("Going to create a game");
+        GameStart();
+    }
+
+    private void GameStart() {
+        Debug.Log("Going to create a game");    
         StartCoroutine("CreateGame");
+
     }
 
     IEnumerator CreateGame() {
@@ -47,6 +55,76 @@ public class GameController : MonoBehaviour
                 var response = req.downloadHandler.text;
                 var game = JsonUtility.FromJson<Game>(response);
                 ShowGameId(game.id);
+                ServerOnline = true;
+                CheckStatsService();
+            }
+        }
+    }
+
+    private void CheckStatsService() {
+        Debug.Log("Checking stats service...");
+        StartCoroutine("CheckStats");
+    }
+
+    IEnumerator CheckStats() {
+        using (UnityWebRequest req = UnityWebRequest.Get(ServerUri + CheckStatsUri)) {
+            yield return req.SendWebRequest();
+
+            if (req.result == UnityWebRequest.Result.ConnectionError || req.result == UnityWebRequest.Result.ProtocolError) {
+                Debug.LogError(req.error);
+            } else {
+                var response = req.downloadHandler.text;
+                var Stats = JsonUtility.FromJson<StatsServiceState>(response);
+                if (Stats.state == "running") {
+                    Debug.Log("Stats service is online");
+                    StatsOnline = true;
+                    InvokeRepeating("StartSendingStats", 0, 5);
+                } else {
+                    Debug.Log("Stats service is offline");
+                }
+                
+            }
+        }
+    }
+
+    private void StartSendingStats()
+    {
+        StartCoroutine("SendStats");
+    }
+
+    IEnumerator SendStats() {
+        if (!(ServerOnline && StatsOnline)) {
+            Debug.Log("Server or Stats service is offline");
+            yield break;
+        };
+
+        Debug.Log("Sending stats...");
+
+        StatsData jsonData = new StatsData(GameId, Mathf.Round(heightToShow * 100f) / 100f);
+
+        // Convert the JSON object to a string
+        string jsonString = JsonUtility.ToJson(jsonData);
+
+        Debug.Log(jsonString);
+
+        using (UnityWebRequest req = new UnityWebRequest(StatsAPIUri, "POST"))
+        {
+            byte[] jsonToSend = System.Text.Encoding.UTF8.GetBytes(jsonString);
+            req.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            req.downloadHandler = new DownloadHandlerBuffer();
+            req.SetRequestHeader("Content-Type", "application/json");
+
+            yield return req.SendWebRequest();
+
+            if (req.result == UnityWebRequest.Result.ConnectionError || req.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(req.error);
+                StatsOnline = false;
+                CancelInvoke("SendStats");
+            }
+            else
+            {
+                Debug.Log("Stats sent successfully");
             }
         }
     }
@@ -57,7 +135,7 @@ public class GameController : MonoBehaviour
         // Add your Camera position update logic here
         var heightToSave = newPosition.y;
 
-        var heightToShow = (heightToSave + 1.012f)*10;
+        heightToShow = (heightToSave + 1.012f)*10;
 
         HeightController.Instance.UpdateHeight(heightToShow);
     }
@@ -72,4 +150,22 @@ public class GameController : MonoBehaviour
 public class Game
 {
     public int id = -1;
+}
+
+public class StatsServiceState
+{
+    public string state = "";
+}
+
+[System.Serializable]
+public class StatsData
+{
+    public int game_id;
+    public float height;
+
+    public StatsData(int gameId, float height)
+    {
+        this.game_id = gameId;
+        this.height = height;
+    }
 }
